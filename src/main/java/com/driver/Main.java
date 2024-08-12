cee3698e5217f93298a4b7808716cec38bc7752e
Vipin Singh <singhvipin123534@gmail.com>
2024-08-12 23:32:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,7 @@
-package com.driver;
-
+// Task 3
 public class Main {
-  
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.meth()); // Calls the meth method inherited from class A
+    }
 }
